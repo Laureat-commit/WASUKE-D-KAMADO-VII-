223732c532b142bada6bece2a6d2132764edd37a
Laureat-commit <freefireneutre23@gmail.com>
2025-04-07 00:48:37 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+require("./Databases/module.js")
+
+//========== Setting Owner ==========//
+global.no = "24177059320"
+global.owner = "𝑾𝑨𝑺𝑼𝑲𝑬 𝑫 𝑲𝑨𝑴𝑨𝑫𝑶 𝑽𝑰𝑰╰⁔╯"
+global.bot = "𝙄𝙈 𝑾𝑨𝑺𝑼𝑲𝑬 𝑫 𝑲𝑨𝑴𝑨𝑫𝑶 𝑽𝑰𝑰╰⁔╯"
+global.v = "1.0.0"
+global.welcome = false
+global.autoread = false
+global.anticall = false
+
+//========= Setting Url Foto =========//
+global.image = "https://qu.ax/jhsus.jpg"
+
+global.msg = {
+"error": "Maaf Adanya Sistem Error Pada Fitur Ini!!",
+"done": "Berhasil🕊", 
+"wait": "Wait To Proses🕊", 
+"owner": "`You Now Owner`", 
+"developer": "`You Now Development`"
+}
+
+
+
+
+
