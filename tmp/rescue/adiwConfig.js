0496e5a902bebdf237dd54ae2ff5d334c19676d8
Laureat-commit <freefireneutre23@gmail.com>
2025-04-07 01:21:42 +0100

    Create adiwConfig.js

---
+++ tmp/rescue/adiwConfig.js
@@ -0,0 +1,13 @@
+module.exports = {
+    approval: {
+        num: "24177059320", // Nomor yang diizinkan untuk menyetujui script
+        text: "setujui", // Kata kunci untuk menyetujui script
+        set: "ANDA HARUS MENDAPATKAN PERSETUJUAN OLEH DANTZY",
+        greet: "ANDA TELAH BERHASIL DI SETUJUI SILAHKAN GUNAKAN SEBAIK MUNGKIN"
+    },
+    creatorScript: '24177059320', // Jangan Hapus Ini
+    filePath: './approval', // Path untuk file approval
+    checkFilePath: './anyaforger.js', // Path untuk file yang dicek integritasnya dan juga itu sesuai kan dengan case milik kalian yak
+    codeToDetect: `main();` // Kode yang dicek integritasnya
+};
+
